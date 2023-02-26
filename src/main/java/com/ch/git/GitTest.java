5b69ed4479c5a087cf7bf1676ff5214077cb6495
Mr·Yao <2543233928@qq.com>
2023-02-26 15:31:37 +0800

    third commit

--- src/main/java/com/ch/git/GitTest.java
+++ src/main/java/com/ch/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("GIt,Hello");
         System.out.println("GIt,Hello2");
+        System.out.println("GIt,Hello3");
     }
 }
