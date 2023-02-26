a10fa4068a92475892222d7084afed747cb1c534
Mr·Yao <2543233928@qq.com>
2023-02-26 15:30:31 +0800

    second commit

--- src/main/java/com/ch/git/GitTest.java
+++ src/main/java/com/ch/git/GitTest.java
@@ -3,5 +3,6 @@ package com.ch.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("GIt,Hello");
+        System.out.println("GIt,Hello2");
     }
 }
