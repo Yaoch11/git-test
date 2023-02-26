20d7c775031f596543d8aaa6c390b1731d6fd9a2
Mr·Yao <2543233928@qq.com>
2023-02-26 16:12:37 +0800

    master commit2

--- src/main/java/com/ch/git/GitTest.java
+++ src/main/java/com/ch/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("GIt,Hello2");
         System.out.println("GIt,Hello3");
         System.out.println("GIt,Hello4");
+        System.out.println("master");
     }
 }
